38ecc516667892d3adc1ef6716ced72dedfa2b33
Simon Adams <simonjadams@gmail.com>
2020-05-25 16:21:13 +0100

    wordlist _form

--- backend/views/wordlist/_form.php
+++ backend/views/wordlist/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use common\models\User;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Wordlist */
@@ -11,11 +13,12 @@ use yii\widgets\ActiveForm;
 <div class="wordlist-form">
 
     <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'user_id')->textInput() ?>
-
-    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
-
+    
+    <?php      
+        echo $form->field($model, 'user_id')->dropdownList(ArrayHelper::map(User::find()->all(), 'id', 'username'),['prompt'=>'Select User']);
+        echo $form->field($model, 'name')->textInput(['maxlength' => true]);               
+    ?>    
+               
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
